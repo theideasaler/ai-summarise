d405067d503fb5b124557c6be05e7cefd35aabd2
theideasaler <theideasaler@gmail.com>
2025-08-21 11:29:20 +1000

    feat: add buttons

--- src/app/multi-functional-input/multi-functional-input.component.ts
+++ src/app/multi-functional-input/multi-functional-input.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-multi-functional-input',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+  ],
   templateUrl: './multi-functional-input.component.html',
-  styleUrl: './multi-functional-input.component.scss'
+  styleUrl: './multi-functional-input.component.scss',
 })
 export class MultiFunctionalInputComponent {
+  inputValue: string = '';
 
+  onSubmit() {
+    console.log('Input value:', this.inputValue);
+    // Add your submit logic here
+  }
+
+  onFineTuned() {
+    console.log('Fine-tuned value:', this.inputValue);
+    // Add your fine-tuned logic here
+  }
 }
